b30d57167d56d8437002e938a59651230d4e7c4d
reuven007 <reuvenmartins44@gmail.com>
2023-05-28 11:07:42 -0300

    Order, Instant part 2

--- src/main/java/com/educandoweb/curso/config/TestConfig.java
+++ src/main/java/com/educandoweb/curso/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.educandoweb.curso.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.curso.entities.Order;
 import com.educandoweb.curso.entities.User;
+import com.educandoweb.curso.repositories.OrderRepository;
 import com.educandoweb.curso.repositories.UserRepository;
 
 @Configuration
@@ -16,6 +19,10 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -23,7 +30,13 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		User u3 = new User(null, "Fred Cardoso", "Fredimm@gmail.com", "98225-5525", "123456");
+		
+		Order o1 = new Order(null, Instant.parse("2023-05-20T120:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2023-05-20T19:53:07Z"), u2);
+//		Order o3 = new Order(null, "Fred Cardoso", "Fredimm@gmail.com", "98225-5525", "123456");
 
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+		orderRepository.saveAll(Arrays.asList(o1,o2));
+
 	}
 }
--- src/main/java/com/educandoweb/curso/resources/OrderResource.java
+++ src/main/java/com/educandoweb/curso/resources/OrderResource.java
@@ -9,29 +9,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.educandoweb.curso.entities.Order;
-import com.educandoweb.curso.services.OrderService;
+import com.educandoweb.curso.entities.User;
+import com.educandoweb.curso.services.UserService;
 
 @RestController
-@RequestMapping(value = "/orders")
+@RequestMapping(value = "/users")
 
-public class UserResource {
-	
+public class OrderResource {
 	@Autowired
-	private OrderService service;
+	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<Order>> findAll() {
-		List<Order> list = service.findAll();
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 
 	}
 	@GetMapping (value = "/{id}")
-	public ResponseEntity<Order> findById(@PathVariable Long id){
-		Order obj = service.findById(id);
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 		
 	}
 	
-
+	
 }
