7d2fc346118219f4fa5b75e8d32415cf8ce8410b
reuven007 <reuvenmartins44@gmail.com>
2023-05-31 22:57:51 -0300

    Category

--- src/main/java/com/educandoweb/curso/config/TestConfig.java
+++ src/main/java/com/educandoweb/curso/config/TestConfig.java
@@ -8,8 +8,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.curso.entities.Category;
 import com.educandoweb.curso.entities.Order;
 import com.educandoweb.curso.entities.User;
+import com.educandoweb.curso.repositories.CategoryRepository;
 import com.educandoweb.curso.repositories.OrderRepository;
 import com.educandoweb.curso.repositories.UserRepository;
 
@@ -23,18 +25,32 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		
+		Category cat1 = new Category(null, "Eletronics", "Artigos eletronicos");
+		Category cat2 = new Category(null, "Books", "Livros gerais");
+		Category cat3 = new Category(null, "Computers", "Pcs, Notebooks, Macs");
+		Category cat4 = new Category(null, "Clothes", "Roupas em geral");
+		Category cat5 = new Category(null, "Instruments", "Classicos, Cordas e Metais");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5));
+	
+		
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		User u3 = new User(null, "Fred Cardoso", "Fredimm@gmail.com", "98225-5525", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2023-05-20T120:53:07Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2023-05-20T20:53:07Z"), u1);
 		Order o2 = new Order(null, Instant.parse("2023-05-20T19:53:07Z"), u2);
-//		Order o3 = new Order(null, "Fred Cardoso", "Fredimm@gmail.com", "98225-5525", "123456");
-
+	
+ 
+		
+	
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 		orderRepository.saveAll(Arrays.asList(o1,o2));
 
---
+++ src/main/java/com/educandoweb/curso/entities/Category.java
@@ -0,0 +1,77 @@
+package com.educandoweb.curso.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String descricao;
+
+	public Category() {
+
+	}
+
+	public Category(Long id, String name, String descricao) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.descricao = descricao;
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
+}
